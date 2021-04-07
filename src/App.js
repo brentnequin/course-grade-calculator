37351f8248b1fec96c4c68b6041577dec00c6e0f
brentnequin <brentnequin@gmail.com>
2021-04-06 22:43:47 -0500

    added basic footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import GradeCalculator from './Components/GradeCalculator';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"><h1>Grade Calculator</h1></header>
-      <main><GradeCalculator /></main>
-      <footer></footer>
+      <header className="App-header">
+        <h1>Grade Calculator</h1>
+      </header>
+      <main>
+        <GradeCalculator />
+      </main>
+      <footer className="App-footer">
+        <br></br>
+        <small>Created by <a href="http://brentnequin.com">Brent Nequin</a></small>
+      </footer>
     </div>
   );
 }
